14484ac1171dcb6a645badfd3145e0d6ac67564c
Pablito Labarta <pablitolabarta@gmail.com>
2019-09-22 13:22:05 -0300

    Volumen estandarizado (amps) y reorg del codigo

--- tedX/tema1_pity.py
+++ tedX/tema1_pity.py
@@ -1,5 +1,52 @@
-
 from .Extensions.Voice import voice
+
+#########
+# UTILS
+notas = (PWalk(4))[:20]
+voice(notas,dur=[1],lyrics=letra5 + letra6 + letra7 + letra8,file="v6",octave=6,scale=Scale.minorPentatonic)
+
+chords = [0,2,3,4]
+duracion = 4
+
+# VOCES
+#utils
+v1.reload()
+v1 >> loop("v6",P[0:16],amp=var([1],4))
+#intro
+v2 >> loop("v6",P[0:4],dur=1,amp=var([3,0],4),mix=0.8,room=0.8,formant=[0])
+#verso
+v3 >> loop("v2",P[0:16],dur=PSum(4,4),amp=var([1,0],8),formant=[1])
+#estribillo
+v3 >> loop("v5",P[0:8],dur=PSum(1,1),amp=var([4],4),mix=0.9,room=0.9,formant=2)
+
+v2 >> loop("v1",P[0:16],dur=PSum(9,4),amp=var([4,0],4),mix=0.9,room=0.9,formant=linvar([0,0]))
+#vx.stop()
+
+#INSTRUMENTIS
+
+#intro
+ps >> saw([0,2], dur=4, sus=3, oct=(5))
+#verso
+m1 >> gong(P+[chords]|P[chords],dur=[duracion/2]*4+[duracion/4]*4, amp=4, oct=(4,[5,6])).every(4,'shuffle').every(2, 'offadd',-1) + var([(0,2,4,6),1])
+
+m2 >> piano(PWalk(4),dur=0.5,amp=1,scale=Scale.minorPentatonic)
+#m1.stop()
+#m2.stop()
+
+# estribillo
+a1 >> gong(PWalk(4),dur=duracion/4,amp=4,scale=Scale.minorPentatonic, sus=duracion/2)
+
+#cierre
+m1 >> piano(chords, dur=duracion, amp=[[1.5]*4+[0]*4]) + var([0,2,4])
+
+#v2.solo()
+#v3.stop()
+
+#p1 >> play("o",rate=-0.5,dur=var([1,0.5,0.25,0.125,0.1],[4]),amp=6)
+#p2 >> play("b",rate=[0.2,0.1,0.05],dur=4,amp=20)
+#v1 >> loop("bomb",P[0:8],amp=10,rate=2)
+
+# AGREGAR SAMPLE DE EXPLOSION
 
 def cambio_de_etapa(f):
     d1.reset()
@@ -10,7 +57,6 @@ def cambio_de_etapa(f):
     m1.stop()
     f()
     print("CAMBIO DE PARTE")
-
 letra1 = "música con computa doras "
 letra2 = "progra mando melo días "
 letra3 = "to dos los días "
@@ -22,10 +68,8 @@ letra8 = "and conversational programming "
 letra9 = "programmers programming programs "
 letra10 = "live coders expose and rewire the innards of software while it generates improvised music and visuals "
 letra11 = "música por computa doras "
-
 Scale.default = 'minor'
 Clock.bpm = 90
-
 def intro(duracion = 1):
     d1 >> play('<-><  K ><Xs  >', dur=duracion, amp=2)
     b1 >> dbass(var([0,2,3,4],4), dur=duracion,sus=0.5, amp=[0.9],oct=5)
@@ -40,76 +84,7 @@ def estribillo(duracion=1):
 def cierre(duracion=4):
     d1 >> play('[bbbb] ',dur=duracion)
     b1 >> dbass([0], dur=duracion, sus=duracion,amp=0.6)
-
 print(Clock.now()%16)
-
-#########
-# UTILS
-notas = (PWalk(4))[:20]
-voice(notas,dur=[1],lyrics=letra5 + letra6 + letra7 + letra8,file="v6",octave=6,scale=Scale.minorPentatonic)
-
-v1.reload()
-v1 >> loop("v6",P[0:16],amp=var([1],4))
-
-# Intro
-
-intro(2)
-
-v2 >> loop("v6",P[0:4],dur=1,amp=var([3,0],4),mix=0.8,room=0.8,formant=[0])
-
-p5 >> saw([0,2,-1,-2], dur=2, sus=1, oct=(5,6))
-
-# Verso
-chords = [0,2,3,4]
-duracion = 4
-
-m1 >> gong(P+[chords]|P[chords],dur=[duracion/2]*4+[duracion/4]*4, amp=4, oct=(4,[5,6])).every(4,'shuffle').every(2, 'offadd',-1) + var([(0,2,4,6),1])
-
-v3 >> loop("v2",P[0:16],dur=PSum(4,4),amp=var([1,0],8),formant=[1])
-
-verso(1)
-
-m2 >> piano(PWalk(4),dur=0.5,amp=1,scale=Scale.minorPentatonic)
-
-# Estribillo
-chords = [0,2,3,4]
-
-estribillo(4)
-
-m2 >> quin(var(chords),oct=5, dur=PDur(7,16), amp=1.5,hpf=linvar([300,1000]), shape=0.5).every(8,'reverse')
-
-x3 >> saw(P+[chords], sus=1, dur=1)
-
-p7 >> play('[####]', dur=duracion, amp=5)
-
-x6 >> blip(0, decay=[0,1])
-
-a1 >> gong(PWalk(4),dur=duracion/4,amp=4,scale=Scale.minorPentatonic, sus=duracion/2)
-
-v3 >> loop("v5",P[0:8],dur=PSum(1,1),amp=var([4],4),mix=0.9,room=0.9,formant=2)
-
-v2 >> loop("v1",P[0:16],dur=PSum(9,4),amp=var([4,0],4),mix=0.9,room=0.9,formant=linvar([0,0]))
-
-# Final
-
-v2.solo()
-
-v3.stop()
-
-m2.stop()
-
-v2.amp = 
-
-cierre(1)
-
-m1 >> piano(chords, dur=duracion, amp=[[1.5]*4+[0]*4]) + var([0,2,4])
-
-#p1 >> play("o",rate=-0.5,dur=var([1,0.5,0.25,0.125,0.1],[4]),amp=6)
-#p2 >> play("b",rate=[0.2,0.1,0.05],dur=4,amp=20)
-#v1 >> loop("bomb",P[0:8],amp=10,rate=2)
-
-# AGREGAR SAMPLE DE EXPLOSION
-
 # INICIAR CANCION
 total = 96
 aviso_tiempo_previo = 32
@@ -118,7 +93,9 @@ def proximo(etapa,tiempo,nombre):
     Clock.schedule(lambda : print(f"faltan {aviso_tiempo_previo//2} beats para el {nombre}"),tiempo - aviso_tiempo_previo//2)
     Clock.schedule(lambda : print(f"faltan {aviso_tiempo_previo} beats para el {nombre}"),tiempo - aviso_tiempo_previo)
     Clock.schedule(lambda : cambio_de_etapa(etapa), tiempo)
+    
 
+#BOTON DE ENCENDIDO
 start = Clock.mod(16) - 0.1
 Clock.schedule(intro, start + total*0)
 # VERSO
--- tedX/tema2_oscuro.py
+++ tedX/tema2_oscuro.py
@@ -16,28 +16,38 @@ Clock.bpm=90
 def intro():
     b1.stop()
     m1 >> soprano(PWalk(8),dur=1,sus=2,amp=1,oct=4)
-    d3 >> play("#",rate=-1/2,dur=8,amp=3)
+    d3 >> play("#",rate=-1/2,dur=8,amp=1)
     m2 >> gong(var([p2.pitch],4), dur=1/4, pan=linvar([-1,1],4), amp=linvar([0,2],8)).sometimes('stutter', 4)
 def verse1():
-    b1 >> sawbass(var([0,1,-1],[12,2,2]),cut=0,sus=0.5, echo=0, echotime=1,decay=0.5,room=0.7, mix=1, amp=[4,2,2,5], dur=1/2,shape=var([0.2,0.3],8), oct=[6,5,5])
+    b1 >> sawbass(var([0,1,-1],[12,2,2]),cut=0,sus=0.5, echo=0, echotime=1,decay=0.5,room=0.7, mix=1, amp=[2,1,1,2.5], dur=1/2,shape=var([0.2,0.3],8), oct=[6,5,5])
 def verse2():
     d2 >> play('V-X-').sometimes('amen')
-    d1 >> play('o|*3|', sample=2, dur=2, amp=4)
+    d1 >> play('o|*3|', sample=2, dur=2, amp=1)
 def bridge():
     b1.solo()
 def prechorus():
     d1.reset()
-    d1 >> play(" X",amp=17)
+    d1 >> play(" X",amp=1)
     d2.reset()
-    d2 >> play("-",amp=10)
-    b1.amp=1.5
+    d2 >> play("-",amp=1)
+    b1.amp=1.3
 def chorus():    
     b1.reset()
-    b1 >> bass(var([0,-1],[8,2]),dur=4,chop=var([8,16],5),amp=3)
+    b1 >> bass(var([0,-1],[8,2]),dur=4,chop=var([8,16],5),amp=1.3, pan=[1,-1])
 
-prechorus()
 
-d1 >> play('[bbbb] ',dur=1)
+# Instrumentis
+
+# bajo del coro con shape
+b1 >> bass(var([0,-1],[8,2]),dur=4,chop=var([8,16],5),amp=1.3, pan=[1,-1], shape=0.2)
+
+#bips del tema anterior
+bb >> play('{[bb] b}{b }{[bb]bb }',dur=1/2, rate=1, amp=PRand(1))
+
+#ambi cosmico # SIRENAAAAAA
+y1 >> ambi(linvar([0,5],16), dur=1/8, cut=0, chop=0, room=0.6, mix=0.8, pan=linvar([-1,1]), amp=linvar([1,1.5,1.5,1]))
+
+d1 >> play('o|*3|', sample=2, dur=2, amp=4)
 
 v1 >> charm(PWalk(5),amp=5,chop=2,scale=Scale.minorPentatonic)
 
@@ -64,7 +74,3 @@ proximo(verse2, start + total*8,"verso2")
 proximo(bridge, start + total*9,"puente")
 proximo(prechorus, start + total*10,"pre-estribillo")
 proximo(chorus, start + total*11,"estribillo")
-
-
-
-
