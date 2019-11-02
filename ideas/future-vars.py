415bd4d6c36672cfc790a6b706cc998b8e2c4a58
Pablito Labarta <pablitolabarta@gmail.com>
2019-11-02 18:03:14 -0300

    Update future-vars.py

--- ideas/future-vars.py
+++ ideas/future-vars.py
@@ -5,7 +5,7 @@ base2=var([0,1,2],[8,2,2])
 def melo1():
     x3 >> saw(base1, dur=1/4, cut=0.7, oct=[4,5,6]) + var([0,1,-2],4)
     x1.dur=1/2
-    print('Va la melodia segunda)
+    print('Va la melodia segunda')
 
 def melo2():
     x3 >> saw(base2, dur=1/4, cut=0.7, oct=[4,5,6]) + var([0,1,-2],4)
