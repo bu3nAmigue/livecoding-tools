b250d1adaa99ac84c309e0b269e92f096b7826a2
Mathi Gatti <mathigatti@gmail.com>
2019-12-28 11:04:45 -0300

    Samples y modificaciones en ritual.py

--- EP/3.ritual/ritual.py
+++ EP/3.ritual/ritual.py
@@ -1,6 +1,6 @@
 Scale.default = "minor"
 Root.default.set("B")
-Clock.bpm=100
+Clock.bpm=80
 
 def note2index(note):
     scale = list(Scale.default)
@@ -24,30 +24,33 @@ def abajoarriba(intervalo):
     abajo()
     Clock.future(intervalo, lambda: arriba())
 
-# Voces
-
-from .Extensions.Voice import voice
-
-voice([0,4,3,2,3,0],dur=[1],lyrics="Tec Flow",file="o2",octave=5,scale=Scale.minorPentatonic)
-
-notas = PWalk(4)[:16]
-voice(notas,dur=[1],lyrics="des mi ti fi can do da ta",file="o1",octave=6,scale=Scale.minorPentatonic)
-
-v2.reload()
-v2 >> loop("o2",P[0:16])
-
-v1.reload()
-v1 >> loop("o1",P[0:16])
-
 ####################
 ##### CANCION ######
 ####################
 
+### VOCES ###
+
+voice([0,0,0,0],dur=[1],lyrics="earth, air, fire, water",file="v2",lang="en",octave=4)
+voice([0,6,4],dur=[1,1,2],lyrics="earth, air, fire",file="oscuro1",lang="en",octave=4)
+voice([3,6,3,2,2,1],dur=[0.5]*4+[1,1],lyrics="my name is good friend",file="oscuro2",lang="en",octave=4)
+voice([6,0,3,5,6,5,6,1,3,5,6,5], dur = ([0.5]*4+[1,1])*2,lyrics="my name is good friend",file="oscuro3",lang="en",octave=4)
+
+v1.reload()
+v1 >> loop('oscuro2',P[4:8], amp=var([2],[1]), formant=var([0],4),chop=0)
+
+v1.reload()
+v1 >> loop('oscuro3',P[4:8], amp=var([2],[1]), formant=var([0],4),chop=0)
+
+v2.reload()
+v2 >> loop('oscuro1',P[4:5], dur=PSum(6,4), amp=var([2],[8]), formant=var([0,[2,4]],4),chop=0, room=0, mix=0)
+
 def intro1():
-    m2 >> gong(var([p2.pitch],4), dur=1/4, pan=linvar([-1,1],4), amp=linvar([0,2],8)).sometimes('stutter', 4)    
+    v1 >> loop('oscuro1',P[4:8], amp=var([1],[1]), formant=var([3],4),room=0.9,mix=0.9)
 def intro2():
-    d2 >> play('w' ,dur=PSum([1,6],4), rate=1/4) 
+    m2 >> gong(var([p2.pitch],4), dur=1/4, pan=linvar([-1,1],4), amp=linvar([0,2],8)).sometimes('stutter', 4)    
 def intro3():
+    d2 >> play('w' ,dur=PSum([1,6],4), rate=1/4) 
+def intro4():
     d3 >> play('K', dur=2, sample=3)
 def verso1():
     m2.stop()
@@ -55,8 +58,9 @@ def verso1():
 def verso2():
     pass
 def verso3():
-    v2 >> loop("o1",P[2:6], lpf=0, rate=[2,0.5], drive=0.3, echo=0, amp=var([0.5,0],4),dur=var([2,4,1],4))
+    v1 >> loop('oscuro3_2',P[6:7], dur=PSum(6,4), amp=var([0,2],[8]), formant=var([0],4),chop=0, room=0, mix=0)
     p1 >> piano(var([0,2],4), drive=0.3, dur=PSum(7,4),amp=0.7,pan=PWhite(-1,1)).every(4,'stutter',2,dur=1) + var([0,2],8)
+    #v2 >> loop("o1",P[2:6], lpf=0, rate=[2,0.5], drive=0.3, echo=0, amp=var([0.5,0],4),dur=var([2,4,1],4))
 def puenteA1():
     d2.solo()
     d3 >> play('---[--]',sample=2,dur=0.5,amp=1)
@@ -68,83 +72,19 @@ def cierre1():
 def cierre2():
     Clock.clear()
 
-verso1()
-
-def suma(v,n):
-    return [note2index((a + n)%7) for a in v]
-
-def arreglo_coral(notas,sample,dur=1,d=0,h=1):
-    x1 >> loop(sample, P[d:h], pshift=var(suma(notas,0),dur))
-    x2 >> loop(sample, P[d:h], pshift=var(suma(notas,2),dur), delay=0)
-    x3 >> loop(sample, P[d:h], pshift=var(suma(notas,4),dur), delay=0)
-    
-print(note2index(5))
-
-print(Scale.minor)
-
-Scale.default='minor'
-arreglo_coral([0,5,4,2],"perro_tuned")
-
-v1 >> loop('perro_tuned', P[0:1], dur=1,rate=1,formant=0, pshift=[0,1,2,3,4,3,2,1], slide=0.0)
-
-v2 >> loop('perro_tuned', P[0:1], dur=1,rate=1,formant=0, pshift=[1,0,1,2,3,4,3,2], delay=0.0)
-
-v2.stop()
-
-v2 >> loop('c2', P[0:16], dur=1,rate=2,formant=2)
-
-v3 >> loop('c1', P[0:16], dur=1,rate=2,formant=2)
-
-v4 >> loop('t1v1', P[0:16], dur=1,rate=1,formant=0)
-
-
-
-puenteA1()
-
-print(SynthDefs)
-
-a1.reset() >> glass([0],oct=4)
-
-
-
-play2notes([0],"1234",dur=[4],ritmo=[2],oct=-1,player=p3)
-
-a4 >> pluck()
-
-abajo()
-
-d3.reset() >> play('K', dur=2, sample=3)
-
-d3.dur=PSum(5,4)
-d3.rate=var([1,2],4)
-
-verso3()
-
-puenteA2()
-
-arriba()
-
-d1 >> play('(X )', sample=6,dur=0.5)
-
-### ESTO DONDE LO METEMOS?
-
-cc >> combs(linvar([0,2],8), amp=0.1, room=0.5).spread()
-
-m2 >> gong(var([p2.pitch],4), dur=1/4, pan=linvar([-1,1],4), amp=linvar([0,2],8)).sometimes('stutter', 4)
-
 ### CANCION ###
 
-intro = [intro1, intro2,intro3]
+intro = [intro1, intro2,intro3,intro4]
 verso = [verso1,verso2,verso3]
 puenteA = [puenteA1,puenteA2]
 cierre = [cierre1, cierre2]
 cancion = intro + verso + puenteA + verso + puenteA + verso + puenteA + verso + cierre
 
-
 ### EFECTOS #####
 
 def efecto1():
     p1 >> piano([0,0,5,5,4,2], drive=0.5, dur=PSum(4,4),amp=0.4,pan=PWhite(-1,1))
+    v1 >> loop('oscuro2_2',P[6:7], dur=PSum(6,4), amp=var([0,2],[8]), formant=var([0],4),chop=0, room=0, mix=0)
 def efecto2():
     d3.reset() >> play('K', dur=2, sample=3)
     d3.dur=PSum(5,4)
@@ -173,5 +113,8 @@ def reproducir(cancion,efectos,reset,start):
             efectos[fname]["efecto"]()
         Clock.schedule(lambda : reproducir(cancion[1:],efectos,reset,start+8),start + 8)
 
-start = Clock.mod(8) - 0.1
-Clock.schedule(lambda : reproducir(cancion,efectos,reset,start), start)
+def arrancarCancion():
+    start = Clock.mod(8) - 0.1
+    Clock.schedule(lambda : reproducir(cancion,efectos,reset,start), start)
+    
+arrancarCancion()
